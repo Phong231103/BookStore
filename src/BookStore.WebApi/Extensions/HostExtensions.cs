2796ff4f0b4a5ab3cfffba8d04a064880a6d9f8c
Nguyen Doan Hoang Phong <nguyenhoangphong2311@gmail.com>
2026-07-21 23:00:17 +0700

    git

---
+++ src/BookStore.WebApi/Extensions/HostExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace BookStore.WebApi.Extensions
+{
+    public static class HostExtension
+    {
+        public static void AddApplicationConfigurations(this WebApplicationBuilder builder)
+        {
+            var env = builder.Environment;
+
+            builder.Configuration
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        }
+    }
+}
