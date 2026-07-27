6bc99b6c1ef85435bfbd35008e04a0e769467c79
PhongNDH <PhongNDH@ics-p.vn>
2026-07-27 17:03:58 +0700

    27/7/2026

--- src/BookStore.Domain/Common/Primitives/AggregateRoot.cs
+++ src/BookStore.Domain/Common/Primitives/AggregateRoot.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BookStore.Domain.Common.Intefaces;
 
-namespace BookStore.Domain.Common.Primitives
+namespace BookStore.Domain.Common.Primitives;
+
+/// <summary>
+/// Represents the base class for all Aggregate Roots.
+/// Aggregate Roots are responsible for recording domain events
+/// that occur during the execution of business rules.
+/// </summary>
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
 {
-    internal class AggregateRoot
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    protected AggregateRoot()
     {
+    }
+
+    protected AggregateRoot(TId id)
+        : base(id)
+    {
+    }
+
+    /// <summary>
+    /// Gets the domain events raised by this aggregate.
+    /// </summary>
+    public IReadOnlyCollection<IDomainEvent> DomainEvents
+        => _domainEvents.AsReadOnly();
+
+    /// <summary>
+    /// Records a new domain event.
+    /// </summary>
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        ArgumentNullException.ThrowIfNull(domainEvent);
+
+        _domainEvents.Add(domainEvent);
+    }
+
+    /// <summary>
+    /// Removes all recorded domain events.
+    /// Called after events have been successfully dispatched.
+    /// </summary>
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
     }
 }
--- src/BookStore.Domain/Common/Primitives/ValueObject.cs
+++ src/BookStore.Domain/Common/Primitives/ValueObject.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BookStore.Domain.Common.Primitives;
 
-namespace BookStore.Domain.Common.Primitives
+/// <summary>
+/// Represents a Value Object in Domain-Driven Design.
+/// Equality is determined by the values of its components,
+/// not by object identity.
+/// </summary>
+public abstract class ValueObject
 {
-    internal class ValueObject
+    /// <summary>
+    /// Returns all components that participate in equality comparison.
+    /// </summary>
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
     {
+        if (obj is null)
+            return false;
+
+        if (ReferenceEquals(this, obj))
+            return true;
+
+        if (obj.GetType() != GetType())
+            return false;
+
+        var other = (ValueObject)obj;
+
+        return GetEqualityComponents()
+            .SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+
+        foreach (var component in GetEqualityComponents())
+        {
+            hash.Add(component);
+        }
+
+        return hash.ToHashCode();
+    }
+
+    public static bool operator ==(
+        ValueObject? left,
+        ValueObject? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(
+        ValueObject? left,
+        ValueObject? right)
+    {
+        return !Equals(left, right);
     }
 }
